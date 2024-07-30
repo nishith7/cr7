a789df908155c68f517622c0a46caca7d3c70dc2
nishith7 <nishith.upadhya@gmail.com>
2024-07-30 23:03:07 +0530

    just do it starting again

---
+++ DataStructuresandalgorithm/LEETcode/minimum_deletion_to_make_string_balanced.cpp
@@ -0,0 +1,27 @@
+//SOLUTION 1: USING STACK 
+// TC: O(N)  SC: O(N)
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        int n = s.length();
+        stack<char> charStack;
+        int deleteCount = 0;
+
+        for(int i = 0; i<n ;i++){
+            if(!charStack.empty() && charStack.top() == 'b' && s[i] == 'a'){
+                charStack.pop();
+                deleteCount++;
+
+            }else{
+                charStack.push(s[i]);
+            }
+
+        }
+return deleteCount;
+        
+    }
+};
