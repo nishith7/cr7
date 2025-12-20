b4056303d3ceb9ec49b31a893ed9ab702efc965d
nishith7 <nishith.upadhya@gmail.com>
2025-12-20 21:31:29 +0530

    20th Dec

---
+++ DataStructuresandalgorithm/LEETcode/delete_columns_to_make_sorted.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+
+        int minCount = 0;
+        for(int j=0;j<strs[0].size();j++){
+            for(int i=0;i<strs.size() - 1;i++){
+                if(strs[i][j] > strs[i+1][j]){
+                    minCount++;
+                    break;
+                }
+            }
+
+
+        }
+        return minCount++;
+        
+    }
+};
